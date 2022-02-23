b1d3832de12562b1a6aa05177092516f3cf39e3f
Mobaswirul Islam <1805090@ugrad.cse.buet.ac.bd>
2022-02-23 10:13:22 +0600

    Changed theme

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import React, {useState} from 'react'
 import Header from "./component/layout/Header";
-import MyRouters from './component/layout/MyRouters'
+import MyRouters from './component/layout/MyRouters';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+
 function App() {
+    const themeOptions = createTheme({
+        palette: {
+            type: 'dark',
+            primary: {
+                main: '#3d3f4a',
+            },
+            secondary: {
+                main: '#f50057',
+            },
+        },
+    });
     return (
-        <div className="App">
-            <Header/>
-            <MyRouters/>
-        </div>
+        <ThemeProvider theme={themeOptions}>
+            <div className="App">
+                <Header/>
+                <MyRouters/>
+            </div>
+        </ThemeProvider>
     );
 }
 
